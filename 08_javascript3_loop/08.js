28c3fd8ae61bff920f3799254e9865ea4bf73e9f
Sumin Kim <cm14121453@gmail.com>
2023-07-11 18:51:51 +0900

    23.07.11
    실습 내용 보충

--- 08_javascript3_loop/08.js
+++ 08_javascript3_loop/08.js
@@ -1,48 +1,47 @@
 function showDan() {
-    document.getElementById('msg').classList.remove('rdan');
-    if (!myform.sel_dan.value) {
-        alert('단을 선택하세요.');
-        myform.sel_dan.focus();
-        return;
-    }
-    var i = myform.sel_dan.value;
-    var str = '';
-    for (var j = 1 ; j <= 9; j++) {
-        str = str + i + ' x ' + j + ' = ' + i*j + '<br>';
-        document.getElementById('msg').innerHTML = str;
-    }
+  if (!myform.sel_dan.value) {
+    alert("단을 선택하세요.");
+    myform.sel_dan.focus();
+    return;
+  }
+  var i = myform.sel_dan.value;
+  var str = "";
+  for (var j = 1; j <= 9; j++) {
+    str = str + i + " x " + j + " = " + i * j + "<br>";
+    document.getElementById("msg").innerHTML = str;
+  }
 }
 // 이미지 랜덤
-function showRandom() {
-    var str2 = '';
-    var i = Math.floor(Math.random()*8)+2;
-    var j = Math.floor(Math.random()*9)+1;
-    var result = i*j;
-    var r10 = Math.floor(result / 10);
-    var r1 = result % 10;
-    str2 = str2 + '<img src="./img/'+ i + '.png">' ;str2 = str2 + '<img src="./img/x.png">';
-    str2 = str2 + '<img src="./img/'+ j + '.png">';
-    str2 = str2 + '<img src="./img/equal.png">'
-    if (r10 == 0) {
-        str2 = str2 + '<img src="./img/blank.png">'
-    }
-    else {
-        str2 = str2 + '<img src="./img/'+ r10 + '.png">'
-    }
-    str2 = str2 + '<img src="./img/'+ r1 + '.png">' + '<br>';
-    document.getElementById('msg').innerHTML = str2;
-}
-
-// 기본 random
 // function showRandom() {
 //     var str2 = '';
 //     var i = Math.floor(Math.random()*8)+2;
 //     var j = Math.floor(Math.random()*9)+1;
-//     str2 = str2 + i + 'x '  + j + ' = ' + i*j + '<br>';
-//     document.getElementById('msg').className = "rdan";
+//     var result = i*j;
+//     var r10 = Math.floor(result / 10);
+//     var r1 = result % 10;
+//     str2 = str2 + '<img src="./img/'+ i + '.png">' ;str2 = str2 + '<img src="./img/x.png">';
+//     str2 = str2 + '<img src="./img/'+ j + '.png">';
+//     str2 = str2 + '<img src="./img/equal.png">'
+//     if (r10 == 0) {
+//         str2 = str2 + '<img src="./img/blank.png">'
+//     }
+//     else {
+//         str2 = str2 + '<img src="./img/'+ r10 + '.png">'
+//     }
+//     str2 = str2 + '<img src="./img/'+ r1 + '.png">' + '<br>';
 //     document.getElementById('msg').innerHTML = str2;
 // }
+
+// 기본 random
+function showRandom() {
+  var str2 = "";
+  var i = Math.floor(Math.random() * 8) + 2;
+  var j = Math.floor(Math.random() * 9) + 1;
+  str2 = "<div class='rdan'>" + i + " x " + j + " = " + i * j + "</div>";
+  document.getElementById("msg").innerHTML = str2;
+}
+
 function showClear() {
-    myform.reset();
-    document.getElementById('msg').innerHTML = "";
+  myform.reset();
+  document.getElementById("msg").innerHTML = "";
 }
